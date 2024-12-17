9e245b5fa8b97bad8901a739af6f72ba7a997c2f
Kawê Cezar <kawecezarjesus@gmail.com>
2024-12-17 16:08:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# curso-c
+ Curso de linguagem C do professor Pietro Martins usando o Devc++
---
+++ ex2.1/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// mostrar mensagens em Linguagem C - printf - saída de dados
+int main() {
+	
+	printf("Oi, tudo bem? tenho 6 anos e programo. \n");
+	
+	printf("Valor inteiro: %d. \n", 10);
+	printf("Valor real: %f. \n", 3.14159265);
+	printf("Valor real com apenas duas casas: %.2f.\n",  3.14159265);
+	printf("Dado de texto: %c. \n", 'a');
+	printf("Dado de texto: %s. \n", "testando");
+	
+	printf("meu nome e: %s.\n", "kawe cezar");
+	system("pause");
+}
