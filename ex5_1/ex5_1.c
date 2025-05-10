415684ceae359e6d003d148afe15a20c44a9e174
Kawê Cezar <kawecezarjesus@gmail.com>
2025-05-10 14:21:09 -0500

    Linguagem C - Aula 5.1 - Domine o comando while - loops/laços condicionais

---
+++ ex5_1/ex5_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+Linguagem C - Aula 5.1 - Domine o comando while - loops/laços condicionais 
+
+*/
+
+int main() {
+    int i = 1;
+
+    while(i <= 10) {
+        printf("%d\n", i);
+        i++;
+    }
+
+    system("pause");
+}
